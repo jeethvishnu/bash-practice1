ba1eeca0fdd2bd5af87a1323afef2cd23f14ae01
vishnu jeeth <jeeth.pam99@gmail.com>
2025-01-11 15:48:08 +0530

    new files

---
+++ backend.sh
@@ -0,0 +1,73 @@
+us=$(id -u)
+time=$(date +%F-%H-%M-%S)
+file=$(echo $0 | cut -d "." -f1)
+log=/tmp/$file-$time.log
+
+if[ us -ne 0 ]
+then
+    echo "is this sudo"
+    exit 1
+else
+    echo "IN SUDO"
+fi
+
+val(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 failed"
+    else
+        echo "$2 success"
+    fi
+}
+
+dnf module list nodejs -y
+val $? "listing"
+
+dnf module disable nodejs:18 -y
+
+dnf module enable nodejs:20 -y
+val $? "enabling and disabling"
+
+dnf install nodejs -y
+val $? "installing"
+
+id expense
+if [ $? -ne 0 ]
+then
+    useradd expense
+    val $? "adding user"
+else
+    echo "already added"
+fi
+
+mkdir -p /app
+val $? "dir created"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+val $? "code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip
+val $? "unzipping"
+
+cd /app
+npm install
+val $? "installing"
+
+cp /home/ec2-user/bash-practice/backend.service /etc/systemd/system/backend.service
+val $? "copying"
+
+systemctl daemon-reload
+systemctl start backend
+systemctl enable backend
+val $? "starting and enabling"
+
+dnf install mysql -y
+val $? "installing"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+val $? "schema"
+
+systemctl restart backend
+val $? "restarting"
---
+++ db.sh
@@ -0,0 +1,39 @@
+u=$(id -u)
+time=$(date +%F-%H-%M-%S)
+script=$(echo $0 | cut -d "." -f1)
+log=/tmp/$script-$time.log
+
+if [ u -ne 0 ]
+then
+    echo "is this sudo"
+    exit 1
+else
+    echo "IN SUDO"
+fi
+
+val(){
+    if [ $1 -ne 0 ]
+    then    
+        echo "$2 failed"
+    else
+        echo "$2 success"
+    fi
+}
+
+dnf install mysql-server -y
+val $? "installing"
+
+systemctl enable mysqld
+
+systemctl start mysqld
+val $? "starting and enabling"
+
+mysql -h db.vjeeth.site -uroot -pExpenseApp@1 -e 'show databases;'
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    val $? "passwd creation"
+else
+    echo "already created skipping"
+fi
+
