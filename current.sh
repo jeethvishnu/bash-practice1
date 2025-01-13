47dda1f583a7bf17effbbea18689a05031b46cee
vishnu jeeth <jeeth.pam99@gmail.com>
2025-01-13 17:06:40 +0530

    new files

--- current.sh
+++ current.sh
@@ -3,7 +3,8 @@ c="hi there from current script"
 echo "calling variable c before caaling other script:$c"
 echo "pid status:$$"
 
-./otherscript.sh
+#./otherscript.sh
+source./otherscript.sh
 
 echo "after calling other script:$c"
 echo "pid status:$$"
